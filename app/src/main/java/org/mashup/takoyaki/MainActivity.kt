2b0d6f8086f60c7ad39c688b1e5a7f3f09dfe1ef
jonghunlee <whdcn@naver.com>
2018-06-30 19:41:45 +0900

    구글맵 적용

--- app/src/main/java/org/mashup/takoyaki/MainActivity.kt
+++ app/src/main/java/org/mashup/takoyaki/MainActivity.kt
@@ -1,12 +1,21 @@
 package org.mashup.takoyaki
 
+import android.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.fragment_map) as SupportMapFragment
+        mapFragment.getMapAsync({
+
+        })
     }
 }
