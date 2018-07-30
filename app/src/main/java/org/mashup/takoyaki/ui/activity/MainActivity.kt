ad3363908ca153e1df5f54ad6a5dd1e1a49e8c3a
jonghunlee <whdcn@naver.com>
2018-07-30 18:13:11 +0900

    impl location permission handler show current location and update

--- app/src/main/java/org/mashup/takoyaki/ui/activity/MainActivity.kt
+++ app/src/main/java/org/mashup/takoyaki/ui/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.mashup.takoyaki.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.ActionBarDrawerToggle
 import dagger.android.support.DaggerAppCompatActivity
@@ -11,6 +12,7 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import org.mashup.takoyaki.ui.fragment.FragmentType
+import org.mashup.takoyaki.ui.fragment.TruckFragment
 
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -29,6 +31,11 @@ class MainActivity : DaggerAppCompatActivity() {
                 .commit()
 
         btReport.setOnClickListener { ReportActivity.start(this) }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        fragmentHolder.getFragment(FragmentType.MAIN)
+                ?.onActivityResult(requestCode, resultCode, intent)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/org/mashup/takoyaki/ui/fragment/TruckFragment.kt
+++ app/src/main/java/org/mashup/takoyaki/ui/fragment/TruckFragment.kt
@@ -1,8 +1,16 @@
 package org.mashup.takoyaki.ui.fragment
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -12,62 +20,226 @@ import com.google.android.gms.maps.model.Marker
 import org.mashup.takoyaki.R
 import javax.inject.Inject
 import com.google.android.gms.maps.model.MarkerOptions
+import com.tbruyelle.rxpermissions2.RxPermissions
 import kotlinx.android.synthetic.main.fragment_truck.*
 import org.mashup.takoyaki.data.remote.model.FoodTruck
 import org.mashup.takoyaki.data.remote.model.FoodTrucks
 import org.mashup.takoyaki.presenter.turck.TruckPresenter
 import org.mashup.takoyaki.presenter.turck.TruckView
 import org.mashup.takoyaki.ui.adapter.TruckPagerAdapter
+import org.mashup.takoyaki.util.SharedPreferencesUtil
+import android.content.IntentSender
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationSettingsStatusCodes
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.location.LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+
 
 /**
  * Created by jonghunlee on 2018-06-30.
  */
-class TruckFragment @Inject constructor() : BaseFragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener, TruckView {
+class TruckFragment @Inject constructor() : BaseFragment(), OnMapReadyCallback, GoogleMap.OnCameraIdleListener, GoogleMap.OnMarkerClickListener, TruckView {
 
     companion object {
         private val TAG = TruckFragment::class.java.simpleName
+
+        private const val REQUEST_CHECK_LOCATION_SETTING = 2001
+
+        private const val MIN_MAP_ZOOM_LEVEL = 15f
+        private const val DEFAULT_LATITUDE = 37.566692
+        private const val DEFAULT_LONGITUDE = 126.978416
     }
+
+    @Inject
+    lateinit var preferencesUtil: SharedPreferencesUtil
 
     @Inject
     lateinit var presenter: TruckPresenter
 
     private lateinit var googleMap: GoogleMap
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+
     private val foodTrucks = mutableListOf<FoodTruck>()
+
+    private var isMyPosition = true
 
     override fun getLayoutId(): Int {
         return R.layout.fragment_truck
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity!!)
+        locationRequest = LocationRequest()
+        setHighAccuracyLocationRequest()
+
         val mapFragment = SupportMapFragment.newInstance()
         fragmentManager?.beginTransaction()?.replace(R.id.mapLayout, mapFragment)?.commit()
+
+        mapLayout.visibility = View.INVISIBLE
+
         mapFragment.getMapAsync(this)
+
         presenter.attachView(this)
 
-        viewPager.clipToPadding = false
-        viewPager.setPadding(64, 0, 64, 0)
-        viewPager.pageMargin = 64
+        vpTrucks.clipToPadding = false
+        vpTrucks.setPadding(64, 0, 64, 0)
+        vpTrucks.pageMargin = 64
+
+        rgDistanceFilter.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.rbOneHandM -> Log.d(TAG, "100M")
+                R.id.rbThreeHandM -> Log.d(TAG, "300M")
+                R.id.rbFiveHandM -> Log.d(TAG, "500M")
+                R.id.rbOneKm -> Log.d(TAG, "1KM")
+            }
+        }
+
+        btMyPosition.setOnClickListener {
+            if (btSearchThisPosition.visibility == View.VISIBLE) {
+                btSearchThisPosition.visibility = View.GONE
+            }
+
+            setHighAccuracyLocationRequest()
+
+            getCurrentLocation()
+        }
+
+        btSearchThisPosition.setOnClickListener {
+            btSearchThisPosition.visibility = View.GONE
+            val centerPosition = googleMap.cameraPosition
+            Log.d(TAG, "Camera position latitude : ${centerPosition.target.latitude}, " +
+                    "longitude : ${centerPosition.target.longitude}")
+        }
+    }
+
+    private fun setHighAccuracyLocationRequest() {
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 5000
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         this.googleMap = googleMap
 
+        googleMap.uiSettings.isTiltGesturesEnabled = false
+        googleMap.uiSettings.isCompassEnabled = false
+        googleMap.uiSettings.isRotateGesturesEnabled = false
+
         googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
 
-        // 서울시청
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(37.566692, 126.978416), 7f))
+        googleMap.setMinZoomPreference(MIN_MAP_ZOOM_LEVEL)
+
+        googleMap.setOnCameraIdleListener(this)
         googleMap.setOnMarkerClickListener(this)
-        presenter.getFoodTrucks()
+
+        checkLocationPermission()
+    }
+
+    private fun checkLocationPermission() {
+        RxPermissions(this)
+                .request(Manifest.permission.ACCESS_FINE_LOCATION,
+                         Manifest.permission.ACCESS_COARSE_LOCATION)
+                .subscribe({
+                               if (it) {
+                                   getLastLocation()
+                               } else {
+                                   Toast.makeText(activity,
+                                                  R.string.permission_location_request_denied,
+                                                  Toast.LENGTH_SHORT).show()
+                                   activity?.finish()
+                               }
+                           }, {
+
+                           })
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        fusedLocationClient.lastLocation?.addOnSuccessListener {
+            if (it != null) {
+                showCurrentLocation(it)
+                getCurrentLocation()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation() {
+        val settingClient = LocationServices.getSettingsClient(activity!!)
+        settingClient.checkLocationSettings(LocationSettingsRequest
+                                                    .Builder()
+                                                    .addLocationRequest(locationRequest!!)
+                                                    .setAlwaysShow(true)
+                                                    .build())
+                .addOnSuccessListener(activity!!) {
+                    fusedLocationClient.requestLocationUpdates(locationRequest,
+                                                               locationCallback,
+                                                               Looper.getMainLooper())
+                }
+                .addOnFailureListener {
+                    failLocationSetting(it)
+                }
+    }
+
+    private fun showCurrentLocation(location: Location) {
+        isMyPosition = true
+        Log.d(TAG,
+              "showCurrentLocation#latitude : ${location.latitude}, longitude : ${location.longitude}")
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(location.latitude,
+                                                                  location.longitude)))
+        mapLayout.visibility = View.VISIBLE
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            showCurrentLocation(locationResult.lastLocation)
+        }
+    }
+
+    private fun failLocationSetting(e: Exception) {
+        val statusCode = (e as ApiException).statusCode
+
+        when (statusCode) {
+            LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> try {
+                val resolvableApiException = e as ResolvableApiException
+                resolvableApiException.startResolutionForResult(activity,
+                                                                REQUEST_CHECK_LOCATION_SETTING)
+            } catch (sie: IntentSender.SendIntentException) {
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQUEST_CHECK_LOCATION_SETTING -> {
+                if (resultCode != Activity.RESULT_OK) {
+                    Log.e(TAG, "User cancel location provide cancel")
+                    when (locationRequest.priority) {
+                        LocationRequest.PRIORITY_HIGH_ACCURACY ->
+                            locationRequest.priority =
+                                    LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+                        LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY ->
+                            locationRequest.priority =
+                                    LocationRequest.PRIORITY_NO_POWER
+                    }
+                }
+
+                getCurrentLocation()
+            }
+        }
     }
 
     override fun resultFoodTrucks(foodTrucks: FoodTrucks) {
         this.foodTrucks.clear()
         this.foodTrucks.addAll(foodTrucks.data)
 
+
+
         val adapter = TruckPagerAdapter()
 
-        viewPager.adapter = adapter
+        vpTrucks.adapter = adapter
         adapter.add(foodTrucks.data)
 
         foodTrucks.data.forEach {
@@ -77,17 +249,21 @@ class TruckFragment @Inject constructor() : BaseFragment(), OnMapReadyCallback, 
         }
     }
 
+    override fun onCameraIdle() {
+        if (isMyPosition) {
+            isMyPosition = false
+            return
+        }
+
+        Log.d(TAG, "onCameraIdle")
+
+        if (btSearchThisPosition.visibility == View.GONE) {
+            btSearchThisPosition.visibility = View.VISIBLE
+        }
+    }
+
     override fun onMarkerClick(marker: Marker): Boolean {
         Log.d(TAG, "market tag : ${marker.tag as String}")
-//        foodTrucks.forEach {
-//            if (it.truckName == marker.tag as String) {
-//                fragmentManager?.beginTransaction()
-//                        ?.setCustomAnimations(R.anim.slide_up, R.anim.slide_down)
-//                        ?.add(R.id.flContents, TruckDetailFragment.newInstance(it.truckName))
-//                        ?.addToBackStack(TruckDetailFragment.TAG)?.commit()
-//                return true
-//            }
-//        }
 
         return true
     }
