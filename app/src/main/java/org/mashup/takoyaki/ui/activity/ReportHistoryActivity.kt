02799e9e43d8fae8b6521beb6fa722420b7bf578
chaeyun <indexor18@gmail.com>
2018-08-05 22:34:24 +0900

    Add ReportHistoryActivity

--- app/src/main/java/org/mashup/takoyaki/ui/activity/ReportHistoryActivity.kt
+++ app/src/main/java/org/mashup/takoyaki/ui/activity/ReportHistoryActivity.kt
@@ -4,23 +4,18 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_my_page.*
 import org.mashup.takoyaki.R
 
-class MyPageActivity : AppCompatActivity() {
+class ReportHistoryActivity : AppCompatActivity() {
 
     companion object {
         fun start(context: Context) {
-            context.startActivity(Intent(context, MyPageActivity::class.java))
+            context.startActivity(Intent(context, ReportHistoryActivity::class.java))
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_page)
-
-        tvBookmark.setOnClickListener { BookmarkActivity.start(this) }
-        tvReportHistory.setOnClickListener { ReportHistoryActivity.start(this) }
+        setContentView(R.layout.activity_report_history)
     }
 }
-
