cab3e2797dd8b62fec098d15b1de62c800498643
jonghunlee <whdcn@naver.com>
2018-08-11 13:23:45 +0900

    fix menu and price layout

--- app/src/main/java/org/mashup/takoyaki/ui/widget/MenuAndPriceLayout.kt
+++ app/src/main/java/org/mashup/takoyaki/ui/widget/MenuAndPriceLayout.kt
@@ -3,7 +3,10 @@ package org.mashup.takoyaki.ui.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.LinearLayout
+import android.widget.Toast
 import kotlinx.android.synthetic.main.item_menu_and_price_list.view.*
 import org.mashup.takoyaki.R
 
@@ -11,39 +14,94 @@ import org.mashup.takoyaki.R
 /**
  * Created by whdcn on 2018-07-27.
  */
-data class MenuAndPrice(val menu: String, val price: Int)
+//data class MenuAndPrice(val menu: String, val price: Int)
 
 
 class MenuAndPriceLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
     : LinearLayout(context, attrs, defStyleAttr) {
 
-    private val menuAndPrices = mutableListOf<MenuAndPrice>()
+    companion object {
+        private const val ACTION_ADD = "ACTION_ADD"
+        private const val ACTION_REMOVE = "ACTION_REMOVE"
+    }
 
     init {
         orientation = LinearLayout.VERTICAL
-
-        val view = LayoutInflater.from(context)
-                .inflate(R.layout.item_menu_and_price_list, this, false)
-        view.btRemove.isEnabled = false
-
-        view.btAdd.setOnClickListener { addItem() }
-
-        addView(view)
+        addView()
     }
 
-    private fun addItem() {
-        val menuAndPrice = MenuAndPrice(etMenu.text.toString(), etPrice.text.toString().toInt())
-        menuAndPrices.add(menuAndPrice)
-        val view = LayoutInflater.from(context)
+    private fun addView() {
+        val view = LayoutInflater
+                .from(context)
                 .inflate(R.layout.item_menu_and_price_list, this, false)
-        view.btRemove.isEnabled = true
-        view.btRemove.setOnClickListener {
-            removeViewAt(menuAndPrices.size)
-            menuAndPrices.removeAt(menuAndPrices.size - 1)
+
+        view.btAddAndRemove.setOnClickListener {
+            when (it.tag) {
+                ACTION_ADD -> {
+                    if (checkInputData(view) && checkInputPrice(view)) {
+                        addView()
+                        hideSoftInput()
+                    }
+                }
+                ACTION_REMOVE -> {
+                    removeView(view.tag as Int)
+                    hideSoftInput()
+                }
+            }
         }
 
-        view.btAdd.setOnClickListener { addItem() }
-
         addView(view)
+        changeButtonAction()
     }
+
+    private fun removeView(position: Int) {
+        removeViewAt(position)
+        changeButtonAction()
+    }
+
+    private fun changeButtonAction() {
+        for (position in 0 until childCount) {
+            val view = getChildAt(position)
+            view.tag = position
+
+            if (position == childCount - 1) {
+                view.btAddAndRemove.text = "+"
+                view.btAddAndRemove.tag = ACTION_ADD
+            } else {
+                view.btAddAndRemove.text = "-"
+                view.btAddAndRemove.tag = ACTION_REMOVE
+            }
+        }
+    }
+
+    private fun hideSoftInput() {
+        val imm =
+                context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
+    }
+
+
+    private fun checkInputData(view: View): Boolean {
+        if (view.etMenu.text.toString().isEmpty() ||
+                view.etPrice.text.toString().isEmpty()) {
+            Toast.makeText(context,
+                           R.string.need_input_menu_and_price,
+                           Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        return true
+    }
+
+    private fun checkInputPrice(view: View): Boolean {
+        if (view.etPrice.text.toString().toIntOrNull() == null) {
+            Toast.makeText(context,
+                           R.string.need_input_price_numeric,
+                           Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        return true
+    }
+
 }
